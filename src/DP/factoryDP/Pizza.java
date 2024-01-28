159b67d8dc41d4c3d38ac618423e45a69ee288b6
GANIDU2413 <ganidusahan@gmail.com>
2024-01-28 23:04:53 +0530

    add class

---
+++ src/DP/factoryDP/Pizza.java
@@ -0,0 +1,12 @@
+package DP.factoryDP;
+
+public abstract class Pizza {
+    public abstract void prepare();
+
+    public abstract void bake();
+
+    public abstract void cut();
+
+    public abstract void box();
+}
+
